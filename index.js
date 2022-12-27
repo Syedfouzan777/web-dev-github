2bae96f976e58bf0e87db883f9423474bcec804e
Syed Fouzan <syedfouzanofficial@gmail.com>
2022-12-27 03:10:35 -0800

    All files added to commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// let newAnchorElement = document.createElement("a");
+
+// let firstParagraph = document.querySelector("p");
+
+// firstParagraph.append(newAnchorElement);
+
+// newAnchorElement.href = "https://www.google.com";
+// newAnchorElement.textContent = "Click Here To Google";
+
+// let newHeadingElement = document.createElement("h1");
+// newHeadingElement.textContent =
+//   "Allah is the greatest of All and Praise be to Allah";
+
+// let headingElement = document.querySelector("p");
+
+// headingElement.append(newHeadingElement);
+
+// let headingElement = document.querySelector("h1");
+
+// headingElement.remove();
+
+let h1 = document.querySelector("h1");
+h1.parentElement.append(h1);
